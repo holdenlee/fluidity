c02c0e45f178a48eba18101fadf71ae2af6ca767
Holden Lee <holdenl@princeton.edu>
2015-06-02 21:55:57 -0400

    Hid the data type of memory of Codelet

--- PTScan.hs
+++ PTScan.hs
@@ -1,3 +1,8 @@
+{-# OPTIONS
+
+  -XExistentialQuantification
+#-}
+
 module GraphUtils where
 import System.Environment
 import Control.Monad
@@ -15,13 +20,17 @@ import System.Random
 import Utilities
 import ParseUtilities
 
+data Codelet' w a = Codelet' { act :: Double -> w -> a -> (Maybe (w, a), [Codelet w]),
+                               _activation :: a -> World w -> Double,
+                               memory :: a}
 
-data Codelet w a = Codelet { act :: (w, a, Double) -> (Maybe (w, a, Double), [Codelet w a]),
-                             activation :: Double,
-                             memory :: a}
+data Codelet w = forall a. (Show a) => Codelet {codelet::Codelet' w a} 
 
-actC :: w -> Codelet w a -> (Maybe (w, a, Double), [Codelet w a])
-actC wk c = act c (wk, memory c, activation c)
+activation :: World w -> Codelet w -> Double
+activation wo c = case c of Codelet c' -> _activation c' (memory c') wo
+
+actC :: World w -> Codelet' w a -> (Maybe (w, a), [Codelet w])
+actC wo c = let m = memory c in act c (_activation c m wo) (workspace wo) m
 
 data IMap v = IMap {_map :: M.Map Int v,
                     _curID :: Int}
@@ -41,15 +50,15 @@ iInserts vals im = foldIterate iInsert vals im
 iDelete :: Int -> IMap v -> IMap v
 iDelete k im = im {_map = _map im |> M.delete k}
 
-type Coderack w a = IMap (Codelet w a)
+type Coderack w = IMap (Codelet w)
 
-codelets :: Coderack w a -> M.Map Int (Codelet w a)
+codelets :: Coderack w -> M.Map Int (Codelet w)
 codelets = _map
 
 --should a concept be able to produce codelets? Or have a codelet that monitors activation of concepts?
 data Concept = Concept { id :: Int,
                          name :: String,
-                         c_activation :: Double
+                         c_activation :: Double,
                          depth :: Double
                        }
 
@@ -57,41 +66,41 @@ type Relation = Concept --no need have a different type
                          
 type Slipnet = G.Gr Concept Relation
 
-data World w a g = World { workspace :: w,
-                           temp :: Double,
-                           coderack :: Coderack w a,
-                           slipnet :: Slipnet,
-                           rng :: g}
+data World w = forall g. RandomGen g => World { workspace :: w,
+                                                temp :: Double,
+                                                coderack :: Coderack w,
+                                                slipnet :: Slipnet,
+                                                rng :: g}
 
 tempActivation :: Double -> Double -> Double
 tempActivation t a = exp ((log a) * t)
 
-z :: Double -> Coderack w a -> Double
-z t crack = L.sum $ L.map (tempActivation t . activation) (M.elems $ codelets crack)
+z :: World w -> Coderack w -> Double
+z wo crack = L.sum $ L.map (tempActivation (temp wo) . activation wo) (M.elems $ codelets crack)
 
-gcfr :: Double -> Double -> [(Int, Codelet w a)] -> Int
-gcfr t rand pairs =
+gcfr :: World w -> Double -> [(Int, Codelet w)] -> Int
+gcfr wo rand pairs =
     case pairs of
       [] -> 0 --uh-oh.
       (i,c):rest -> 
           let 
-              energy = tempActivation t $ activation c
+              energy = tempActivation (temp wo) $ activation wo c
           in
             if rand < energy
             then i
-            else gcfr t (rand - energy) rest
+            else gcfr wo (rand - energy) rest
         
 
-getCodeletFromRandom :: Double -> Double -> Coderack w a -> Int
-getCodeletFromRandom t rand crack = gcfr t rand $ M.assocs $ codelets crack
+getCodeletFromRandom :: World w -> Double -> Coderack w -> Int
+getCodeletFromRandom wo rand crack = gcfr wo rand $ M.assocs $ codelets crack
 
-pickCodelet :: (RandomGen g) => g -> Double -> Coderack w a -> (g, Int)
-pickCodelet gen t crack =
+pickCodelet :: (RandomGen g) => g -> World w -> Coderack w -> (g, Int)
+pickCodelet gen wo crack =
     let
-        zTotal = z t crack
+        zTotal = z wo crack
         (r, newGen) = randomR (0, zTotal) gen 
     in
-      (newGen, getCodeletFromRandom t r crack)
+      (newGen, getCodeletFromRandom wo r crack)
 
 {-
 World { workspace :: w,
@@ -100,18 +109,18 @@ World { workspace :: w,
                            slipnet :: Slipnet,
                            rng :: g}
 -}
-execCodelet :: (RandomGen g) => Int -> World w a g -> Coderack w a -> World w a g
+execCodelet :: Int -> World w -> Coderack w -> World w
 execCodelet i world crack =
-    let
-        c = lookup2 i $ codelets crack
-        (updated, children) = actC (workspace world) c
-    in
-      --case c of 
-      --  Nothing -> (world, crack) --shouldn't happen
-      case updated of
-        Nothing -> world {coderack = crack |> iDelete i |> iInserts children}
-        Just (newWorkspace, newMem, newActiv) -> 
-                   world {workspace = newWorkspace,
-                          coderack = crack |> iInsertK i (c{activation = newActiv,
-                                                           memory = newMem})
-                                           |> iInserts children}
+    case lookup2 i $ codelets crack of
+      Codelet c' ->
+          let
+              (updated, children) = actC world c'
+          in
+            --case c of 
+            --  Nothing -> (world, crack) --shouldn't happen
+            case updated of
+              Nothing -> world {coderack = crack |> iDelete i |> iInserts children}
+              Just (newWorkspace, newMem) -> 
+                  world {workspace = newWorkspace,
+                         coderack = crack |> iInsertK i (Codelet (c'{memory = newMem}))
+                                          |> iInserts children}
